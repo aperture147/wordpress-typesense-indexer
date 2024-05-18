ade0b4d1000aa38ff502ced587621191ab979432
HaiVQ <me@haivq.com>
2024-05-18 22:14:15 +0700

    read csv

--- indexer.py
+++ indexer.py
@@ -4,6 +4,7 @@ import typesense
 from phpserialize import loads
 import os
 import json
+import csv
 
 config = ConfigParser()
 config.read('config.ini')
@@ -15,13 +16,13 @@ typesense_config = config['typesense']
 wordpress_host = wordpress_config['host']
 
 db_conn = pymysql.connect(
-    host=mysql_config['host'], port=mysql_config['port'],
+    host=mysql_config['host'], port=int(mysql_config['port']),
     user=mysql_config['user'], passwd=mysql_config['password'], 
     db=mysql_config['db_name'],
     connect_timeout=30,
     autocommit=False
 )
-
+print('db connected')
 typesense_client = typesense.Client({
   'nodes': [{
     'host': typesense_config['host'],  # For Typesense Cloud use xxx.a1.typesense.net
@@ -31,7 +32,7 @@ typesense_client = typesense.Client({
   'api_key': typesense_config['api_key'],
   'connection_timeout_seconds': 3600
 })
-
+print('typesense client created')
 def get_new_posts(post_id_chunk: list):
     
     with db_conn.cursor() as cur:
@@ -42,6 +43,7 @@ def get_new_posts(post_id_chunk: list):
         """)
         
         term_taxonomy_result = cur.fetchall()
+        print('all terms fetched')
         term_taxonomy_dict = {
             term_id: taxonomy_id
             for term_id, taxonomy_id, *_ in term_taxonomy_result
@@ -50,60 +52,74 @@ def get_new_posts(post_id_chunk: list):
             taxonomy_id: (term_name, term_slug, taxonomy_name, term_taxonomy_dict.get(parent_id))
             for _, taxonomy_id, parent_id, term_name, term_slug, taxonomy_name in term_taxonomy_result
         }
-
+        print('taxonomy adjacent list built')
         cur.execute("""
             SELECT object_id, term_taxonomy_id
             FROM wp0e_term_relationships
             WHERE object_id IN %s
         """, (post_id_chunk,))
-        term_relationship_dict = dict(cur.fetchall())
+        term_relationship_list = cur.fetchall()
+        print('term relationship fetched')
+        print(term_relationship_list)
         post_taxonomy_dict = {}
-        for post_id, taxonomy_id in term_relationship_dict.items():
+        for post_id, taxonomy_id in term_relationship_list:
             post_taxonomy = post_taxonomy_dict.setdefault(post_id, {})
             _, _, taxonomy_name, _ = taxonomy_dict[taxonomy_id]
             post_taxonomy.setdefault(taxonomy_name, []).append(taxonomy_id)
-
+        print(post_taxonomy_dict)
+        
+        print('post taxonomy list built')
         cur.execute("""
             SELECT
                 p.id, p.comment_count, u.user_nicename, p.post_content,
                 p.post_date, p.post_excerpt, p.post_modified, p.post_title,
-                p.post_type, p_thumb.guid, p_thumb_m.meta_value
+                p.post_type, p_thumb.guid, p_thumb_m.meta_value, p_category_m.meta_value, p.guid, p.post_name
             FROM wp0e_posts AS p
             LEFT JOIN wp0e_postmeta AS pm ON pm.post_id = p.ID AND pm.meta_key = '_thumbnail_id'
+            LEFT JOIN wp0e_postmeta AS p_category_m ON p_category_m.post_id = p.ID AND p_category_m.meta_key = '_primary_term_category'
             LEFT JOIN wp0e_posts AS p_thumb ON CAST(pm.meta_value AS INTEGER) = p_thumb.ID
             LEFT JOIN wp0e_postmeta as p_thumb_m on p_thumb_m.post_id = p_thumb.ID AND p_thumb_m.meta_key = '_wp_attachment_metadata'
             LEFT JOIN wp0e_users as u on u.ID = p.post_author
             WHERE p.id IN %s
         """, (post_id_chunk,))
-
+        print('post fetched')
         post_list = cur.fetchall()
 
     typesense_list = []
     for id, comment_count, post_author, \
         post_content, post_date, post_excerpt, \
         post_modified, post_title, post_type, \
-        thumb_url, thumb_meta_str in post_list:
-        post_taxonomy = post_taxonomy[id]
+        thumb_url, thumb_meta_str, category_id_str, post_link, post_name in post_list:
+        print('processing post', id)
+        current_post_taxonomy = post_taxonomy_dict[id]
+        if category_id_str:
+            category_id = int(category_id_str)
+        else:
+            category_id = int(current_post_taxonomy.get('category', [0])[0])
+
+
         
         category = []
         cat_link = []
-        category_tax_id_list = post_taxonomy.get('category', [])
-        permalink = ''
-        for tax_id in category_tax_id_list:
-            cat_link_part = []
-            parent_id = 69 # init
+        
+        permalink = post_link
+
+        cat_link_part = []
+        if category_id:
+            parent_id = category_id # init
             while parent_id:
-                term_name, term_slug, _, parent_id = taxonomy_dict[tax_id]
+                term_name, term_slug, _, parent_id = taxonomy_dict[parent_id]
                 category.append(term_name)
                 cat_link_part.append(term_slug)
             if cat_link_part:
                 cat_link_part.reverse()
                 cat_link.append(os.path.join(wordpress_host, 'category', *cat_link_part))
-                permalink = os.path.join(wordpress_host, *cat_link_part, post_author)
-
+                permalink = os.path.join(wordpress_host, *cat_link_part, post_name, post_author)
+        print('permalink', permalink)
+        
         tag = []
         tag_link = []
-        tag_tax_id_list = post_taxonomy.get('post_tag', [])
+        tag_tax_id_list = current_post_taxonomy.get('post_tag', [])
         for tax_id in tag_tax_id_list:
             term_name, term_slug, _, parent_id = taxonomy_dict[tax_id]
             tag.append(term_name)
@@ -136,10 +152,20 @@ def get_new_posts(post_id_chunk: list):
     with open('result.json', 'w') as f:
         json.dump(typesense_list, f)
 
-    typesense_client.collections['post'].documents.import_(typesense_list, {'action': 'upsert'})
+    # typesense_client.collections['post'].documents.import_(typesense_list, {'action': 'upsert'})
+
+def get_post_id():
+    with open('data.csv') as csv_f:
+        reader = csv.reader(csv_f)
+        next(reader, None)
+        return [*set(post_id for post_id, *_ in reader)]
 
 def main():
-    get_new_posts([1956456, 1956454, 1956451, 1956449, 1956447])
+    post_id_list = get_post_id()
+    get_new_posts(post_id_list)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    finally:
+        db_conn.close()
